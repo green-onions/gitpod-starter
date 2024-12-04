b8c811dfd6df8b178f5a91f4d4c0eb6aa4f0d060
Marien Regnault <61194210+green-onions@users.noreply.github.com>
2024-12-03 19:01:10 -0500

    Update setup.sh

--- project/setup.sh
+++ project/setup.sh
@@ -3,6 +3,6 @@
 /usr/local/bin/gitpod-setup-ddev.sh
 composer install
 # Regular install
-# ddev exec drush si --existing-config --account-pass=admin -y
+ddev exec drush si --existing-config --account-pass=admin -y
 # Umami install
-ddev exec drush si demo_umami --account-name=admin --account-pass=admin -y
+# ddev exec drush si demo_umami --account-name=admin --account-pass=admin -y
